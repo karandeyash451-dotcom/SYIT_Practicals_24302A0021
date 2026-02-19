2c423e44422551b05ee268195bcf9ce04a712dfd
karandeyash451-dotcom <karandeyash451@gmail.com>
2026-02-19 11:33:58 +0530

    Create main_method.java

---
+++ Practical_0011/main_method.java
@@ -0,0 +1,20 @@
+package practical_07;
+public class Practical_07 {
+ public static void main(String[] args) {
+ Student_Member s1 = new Student_Member("24302A0021","YASH KARANDE");
+ s1.displayDetails();
+ Faculty_Member f1 = new Faculty_Member("0001", "KETAKI");
+ f1.displayDetails();
+ }
+
+}
+package practical_07;
+public class Practical_07 {
+ public static void main(String[] args) {
+ Student_Member s1 = new Student_Member("24302A0021","YASH KARANDE");
+ s1.displayDetails();
+ Faculty_Member f1 = new Faculty_Member("0001", "KETAKI");
+ f1.displayDetails();
+ }
+
+}
