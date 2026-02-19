325e7867f707127974896323dcd998b3bcce37d1
karandeyash451-dotcom <karandeyash451@gmail.com>
2026-02-19 11:05:05 +0530

    Delete Member.java

--- Member.java
+++
@@ -1,9 +0,0 @@
-package practical_07;
-abstract public class Member {
- String Id,name;
- public Member(String Id, String name) {
- this.Id = Id;
- this.name = name;
- }
- abstract double calculateFine(int no_of_days);
-}
